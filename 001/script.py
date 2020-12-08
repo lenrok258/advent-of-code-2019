52f082c89b700785f81ec41100f1c21487cbacf9
kornel.zemla <kornel.zemla@ocado.com>
2020-12-08 16:53:58 +0100

    Day 1, star 1

--- 001/script.py
+++ 001/script.py
@@ -1,3 +1,6 @@
-lines = open('input.txt', 'r').read().splitlines()
+from math import floor
+from functools import reduce
 
-
+masses = map(int, open('input.txt', 'r').read().splitlines())
+result = reduce(lambda a, b : a + floor(b / 3) - 2, masses, 0)
+print(result)
